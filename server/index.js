9d6b472c603a8e14ee9ffcbc7a317536a3bf59b0
Shubham Kumar <114337784+shubh1777@users.noreply.github.com>
2023-10-17 23:31:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app=express();
 
 app.use(cors(
   {
-        origin:["https://file-upload-kohl.vercel.app"],
+        origin:["https://file-upload-gules.vercel.app/"],
         methods:["POST","GET"],
         credentials:true
     }
